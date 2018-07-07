35d4b70d18486303a68c9fe963fede4fa5d2a4ff
aamir0688 <aamir_1388@hotmail.com>
2018-07-07 23:36:41 +0400

    Finish Doorkeeper Implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # skip_before_action :verify_authenticity_token
+  helper_method :current_user
+  
+  rescue_from OAuth2::Error do |exception|
+    puts exception.response.status
+    if exception.response.status == 401
+      session[:user_id] = nil
+      session[:access_token] = nil
+      redirect_to new_session_path, alert: "Try signing in again."
+    end
+  end
+
+
+  private
+  def current_user
+    if doorkeeper_token
+      @current_user ||= User.find_by(id: doorkeeper_token.resource_owner_id)
+    else
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :send_code, :verify_code, :resend]
   before_action :authorize, except: [:index]
 
   def authorize
@@ -8,10 +8,16 @@ class BlogsController < ApplicationController
     end 
   end  
 
-  # GET /blogs
-  # GET /blogs.json
   def index
-    @blogs = Blog.all
+
+    @blogs = Blog.published
+
+  end
+
+  def my_blog_list
+
+    @blogs = Blog.where(user_id: current_user)
+
   end
 
   # GET /blogs/1
@@ -32,27 +38,66 @@ class BlogsController < ApplicationController
   # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
-
+    @user = current_user
+    send_token = TwilioAuthy.new(@user).send_code_authy(@user.authy_id)
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
+        flash[:notice] = 'Blog has been saved'
+        format.js  { render :send_code, locals: {blog_id: @blog.id} }
       else
-        format.html { render :new }
+        format.js { render :new, locals: {blog_id: @blog.id} }
         format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
+
+  def send_code
+    @user = current_user
+    send_token = TwilioAuthy.new(@user).send_code_authy(@user.authy_id)
+    respond_to do |format|
+      if send_token
+        flash[:notice] = 'Code has been sent'
+        format.js  { render :send_code, locals: {blog_id: @blog.id} }
+      else
+        format.js { render :new, locals: {blog_id: @blog.id} }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  
+  def verify_code
+    @user = current_user
+    # Use Authy to send the verification token
+    @authy_signup = TwilioAuthy.new(@user).verify_code_authy(@user.authy_id, params[:token] )
+    if @authy_signup
+      # Mark the user as verified for get /user/:id
+      @blog.update(verify: true)
+      @blog.update(article_status: "Published")
+      flash[:notice] = 'Article has been published'
+    else
+      flash.now[:danger] = "Incorrect code, please try again"
+      render :send_code, locals: {blog_id: @blog.id}
+    end
+  end
+
+  def resend
+    @user = current_user
+    TwilioAuthy.new(@user).send_code_authy(@user.authy_id)
+    flash[:notice] = 'Verification code re-sent'
+    render :send_code, locals: {blog_id: @blog.id}
+  end  
 
   # PATCH/PUT /blogs/1
   # PATCH/PUT /blogs/1.json
   def update
     respond_to do |format|
       if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
+        format.js { render :show, locals: {blog_id: @blog.id, access_token: session[:access_token]} , notice: 'Blog was successfully updated.' }
+        flash[:notice] = 'Article has been updated'
       else
-        format.html { render :edit }
+        flash[:notice] = 'Article has not been updated'
+        format.js { render :edit }
         format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
@@ -63,7 +108,7 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url(access_token: session[:access_token]), notice: 'Blog was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -76,6 +121,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:user_id, :title, :description, :content, :article_status, :verify, :users_id)
+      params.require(:blog).permit(:user_id, :title, :description, :content, :article_status, :verify)
     end
 end

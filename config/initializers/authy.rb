47a6501211046b57fedecc2194dce38b402eb609
aamir0688 <aamir_1388@hotmail.com>
2018-07-08 22:20:37 +0400

    ADD ENV variables for Authy

--- config/initializers/authy.rb
+++ config/initializers/authy.rb
@@ -1,4 +1,4 @@
 require 'authy'
 
-Authy.api_key = Rails.application.secrets.authy_key
-Authy.api_uri = Rails.application.secrets.api_uri
+Authy.api_key = ENV["authy_api_key"]
+Authy.api_uri = ENV["authy_api_uri"] 

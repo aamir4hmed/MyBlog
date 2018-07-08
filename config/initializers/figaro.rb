f0f522b62315396f225ffbbdd115e83444860c3b
aamir0688 <aamir_1388@hotmail.com>
2018-07-08 08:22:56 +0400

    Environment variabled required

---
+++ config/initializers/figaro.rb
@@ -0,0 +1,32 @@
+# Here you can find all required environment
+# variables to get this app up and running.
+
+# Don't run figaro for tests as we keep
+# getting Figaro::MissingKeys error.
+unless %w(test).include? Rails.env
+
+  # Essentials
+
+    # Twilio-Authy
+    'authy_api_key',
+    'authy_api_uri'
+
+
+    # google_oauth2
+    'oauth_app_id',
+    'oauth_secret',
+    'google_app_id',
+    'google_secret',
+
+    # facebook
+    'oauth_app_id',
+    'oauth_secret',
+    'facebook_app_id',
+    'facebook_secret'
+
+    # Doorkeeper
+    'oauth_redirect_uri',
+    'server_base_url',
+
+  )
+end
